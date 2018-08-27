65f9cfcd5520ecdc81ba4f3b5a1655d4590ded06
ceramic <ceramic.yu@foxmail.com>
2018-08-27 01:39:43 -0700

    init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/websocket"
+	"imp"
+	"fmt"
+)
+var  Poll []*imp.Wsmsg
+
+func wsHandler(w http.ResponseWriter , r *http.Request){
+	fmt.Println(r.RemoteAddr)
+	up:=&websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	var (
+		wsconn *websocket.Conn
+		err error
+
+	)
+
+	if wsconn,err= up.Upgrade(w,r,nil);err!=nil{
+		return
+	}
+	ws:=imp.Initwsmsg(wsconn)
+	Poll=append(Poll, ws)
+
+	go ws.WriteToAll(&Poll)
+	go ws.Read()
+
+	return
+}
+
+func main(){
+	http.HandleFunc("/ws", wsHandler)
+	http.ListenAndServe(":5555",nil)
+}
