b2502e79882d6f857a63b2e3889133cc3d4051c8
ceramicyu <31750600+ceramicyu@users.noreply.github.com>
2019-02-21 16:11:46 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 
 func main(){
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		io.WriteString(writer,"Hi,this is a new world! update 2019")
+		io.WriteString(writer,"Hi,this is a new world! update 2019 2 ")
 	})
 	http.HandleFunc("/git", func(writer http.ResponseWriter, request *http.Request) {
-		shellPath := "/home/www/web/web_s/r.sh"
+		shellPath := "$GOPATH/src/r.sh"
 		command := exec.Command(shellPath) //初始化Cmd
 		err := command.Start()//运行脚本		
 		if err != nil {
