42419c2e00b7b5103e1a144093857e3551144333
ceramicyu <31750600+ceramicyu@users.noreply.github.com>
2019-02-21 15:04:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"net/http"
-	"os/exec"
 	"io"
+	"net/http"
+	"os"
 )
 
 func main(){
@@ -13,18 +12,24 @@ func main(){
 		io.WriteString(writer,"Hi,this is a new world! update 2019")
 	})
 	http.HandleFunc("/git", func(writer http.ResponseWriter, request *http.Request) {
-		cmd := exec.Command("/bin/bash", "-c", "/home/www/web/web_s/r.sh")
-		var out bytes.Buffer
-
-		cmd.Stdout = &out
-		err := cmd.Run()
+		shellPath := "/home/www/web/web_s/r.sh"
+		argv := make([]string, 1)
+		attr := new(os.ProcAttr)
+		newProcess, err := os.StartProcess(shellPath, argv, attr)  //运行脚本
 		if err != nil {
-			fmt.Println("git pull err :",err)
-			io.WriteString(writer,"git pull success"+fmt.Sprintf("%v",err))
+			fmt.Println(err)
+		}
+		fmt.Println("Process PID", newProcess.Pid)
+		processState, err := newProcess.Wait() //等待命令执行完
+		if err != nil {
+			fmt.Println(err)
+			io.WriteString(writer,"git pull err :"+fmt.Sprintf("%v",err))
 		}else{
 			io.WriteString(writer,"git pull success")
 		}
-
+		fmt.Println("processState PID:", processState.Pid())//获取PID
+		fmt.Println("ProcessExit:", processState.Exited())//获取进程是否退出
+		
 	})
 	http.ListenAndServe(":8080",nil)
 }
