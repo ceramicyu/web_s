d4999e2c894e6b99e60a37cf884a3aba5c51b1da
ceramicyu <31750600+ceramicyu@users.noreply.github.com>
2019-02-21 16:15:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,26 +4,21 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os/exec"
 )
 
 func main(){
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		io.WriteString(writer,"Hi,this is a new world! update 2019 2 ")
+		io.WriteString(writer,"Hi,this is a new world! update 2019")
 	})
 	http.HandleFunc("/git", func(writer http.ResponseWriter, request *http.Request) {
-		shellPath := "$GOPATH/src/r.sh"
-		command := exec.Command(shellPath) //初始化Cmd
-		err := command.Start()//运行脚本		
-		if err != nil {
-			fmt.Println(err)
+
+         _,err:=  http.Get("http://106.13.60.133:55555/")
+		if err != nil {			
 			io.WriteString(writer,"git pull err :"+fmt.Sprintf("%v",err))
 		}else{
 			io.WriteString(writer,"git pull success")
 		}
-		
 
 	})
 	http.ListenAndServe(":8080",nil)
 }
-
