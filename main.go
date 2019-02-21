93ef4703e365e6ff7c4eef6b0736a6ab24f78d25
ceramicyu <31750600+ceramicyu@users.noreply.github.com>
2019-02-21 14:34:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main(){
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		io.WriteString(writer,"Hi,this is a new world! update")
+		io.WriteString(writer,"Hi,this is a new world! update 2")
 	})
 	http.HandleFunc("/git", func(writer http.ResponseWriter, request *http.Request) {
 		cmd := exec.Command("/bin/bash", "-c", "")
